305f18487e202a6d24ed5d88891e82c5f818b71f
elvisguillen <hello@elvisguillen.com>
2018-09-10 14:28:51 -0400

    Cleaned up empty files

--- src/app/data/store.js
+++ src/app/data/store.js
@@ -11,33 +11,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 import { persistToken } from './middleware';
 
-const middleware = composeWithDevTools(applyMiddleware(routerMiddleware(history), thunk, createLogger(), persistToken));
+const middleware = composeWithDevTools(
+  applyMiddleware(
+    routerMiddleware(history), 
+    thunk, createLogger(), 
+    persistToken
+  )
+);
 
 const store = createStore(connectRouter(history)(reducer), middleware);
 
 export default store;
-
-// store.subscribe(() => {
-
-//   localStorage.setItem('TOKEN', store.getState().auth);
-// });
-
-// store.subscribe(() => {
-//   console.log('Store Changed', store.getState());
-// })
-
-// store.dispatch({
-//   type: 'FETCH_USERS',
-//   payload: axios.get('https://jsonplaceholder.typicode.com/users');
-// })
-
-// store.dispatch((dispatch) => {
-//   dispatch({type: 'FETCH_USERS_START'})
-//   axios.get("http://rest.learncode.academy/api/wstern/users")
-//     .then((response) => {
-//       dispatch({type: 'RECEIVE_USERS', payload: response.data})
-//     })
-//     .then((err) => {
-//       dispatch({type: 'FETCH_USERS_ERROR', payload: err})
-//     }) 
-// })
